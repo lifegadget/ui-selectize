ee5bc065ec9b4853481d2bdb04b799f0ba9af69a
Ken Snyder <ken@ken.net>
2015-06-11 10:54:56 +0100

    ember-cli-mirage in place for testing

---
+++ app/mirage/fixtures/animals.js
@@ -0,0 +1,5 @@
+export default [
+  {id:'monkey', name:'Monkey', group:'animal', synonyms: ['ape','gorilla']},
+  {id:'lion', name:'Lion', group:'animal', synonyms: 'king'},
+  {id:'rabbit', name:'Rabbit', group:'animal'}
+];
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,40 +2,35 @@ import Ember from 'ember';
 const { computed, observer, $, A, run, on, typeOf, debug, keys, get, set, inject } = Ember;    // jshint ignore:line
 
 export default Ember.Controller.extend({
-	demoChoices: ['Frog','Monkey','Lion'],
+  demoChoices: ['Frog','Monkey','Lion'],
   myValue: 'bar',
   myStartingTags: ['foo','baz'],
-	mySelection: 'Monkey',
-	myTags: ['Monkey'],
-	groupedChoices: [
-		{id:'arm', name:'Arm', group:'body'},
-		{id:'leg', name:'Leg', group:'body'},
-		{id:'chest', name:'Chest', group:'body'},
-		{id:'red', name:'Red', group:'color'},
-		{id:'blue', name:'Blue', group:'color'},
-		{id:'green', name:'Green', group:'color'},
-		{id:'monkey', name:'Monkey', group:'animal'},
-		{id:'lion', name:'Lion', group:'animal'},
-		{id:'rabbit', name:'Rabbit', group:'animal'}
+  mySelection: 'Monkey',
+  myTags: ['Monkey'],
+  groupedChoices: [
+    {id:'arm', name:'Arm', group:'body'},
+    {id:'leg', name:'Leg', group:'body'},
+    {id:'chest', name:'Chest', group:'body'},
+    {id:'red', name:'Red', group:'color'},
+    {id:'blue', name:'Blue', group:'color'},
+    {id:'green', name:'Green', group:'color'},
+    {id:'monkey', name:'Monkey', group:'animal'},
+    {id:'lion', name:'Lion', group:'animal'},
+    {id:'rabbit', name:'Rabbit', group:'animal'}
    ],
-	optGroup: [
-		{id: 'color', name: 'Colors'},
-		{id: 'animal', name: 'Animals'},
-		{id: 'body', name: 'Body'}
-	],
-	animals: [
-		{id:'monkey', name:'Monkey', group:'animal', synonyms: ['ape','gorilla']},
-		{id:'lion', name:'Lion', group:'animal', synonyms: 'king'},
-		{id:'rabbit', name:'Rabbit', group:'animal'}
-	],
-	isolate: false,
+  optGroup: [
+    {id: 'color', name: 'Colors'},
+    {id: 'animal', name: 'Animals'},
+    {id: 'body', name: 'Body'}
+  ],
+  animals: [
+    {id:'monkey', name:'Monkey', group:'animal', synonyms: ['ape','gorilla']},
+    {id:'lion', name:'Lion', group:'animal', synonyms: 'king'},
+    {id:'rabbit', name:'Rabbit', group:'animal'}
+  ],
+  edAnimals: computed(function() {
+    return this.store.find('animal');
+  }),
+  isolate: false,
   disabledToggle: true
-	// promisedOptions: computed(function() {
-	// 	var store = this.get('store');
-	// 	return store.find('color');
-	// }),
-	// promisedObject: computed(function() {
-	// 	var store = this.get('store');
-	// 	return store.find('animal', 'mammal');
-	// })
 });
